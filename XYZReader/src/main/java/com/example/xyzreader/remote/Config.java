867440e6f1f19d20d5c921b4445bb361bbcb8f2f
melle095 <melle095@gmail.com>
2022-10-03 22:46:13 +0300

    Initial commit

---
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -0,0 +1,25 @@
+package com.example.xyzreader.remote;
+
+import android.util.Log;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Config {
+    public static final URL BASE_URL;
+    private static String TAG = Config.class.toString();
+
+    static {
+        URL url = null;
+        try {
+            url = new URL("https://raw.githubusercontent.com/TNTest/xyzreader/master/data.json" );
+        } catch (MalformedURLException ignored) {
+            // TODO: throw a real error
+            Log.e(TAG, "Please check your internet connection.");
+            throw new RuntimeException("Bad URL");
+
+        }
+
+        BASE_URL = url;
+    }
+}
